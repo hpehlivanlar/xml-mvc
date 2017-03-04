501f00791511b7feb1b6706cd995756d198af014
hpehlivanlar <hpehlivanlar@gmail.com>
2017-03-04 16:07:40 +0200

    Add files via upload

---
+++ Controllers/GaleriController.cs
@@ -0,0 +1,22 @@
+﻿using fotogaleri.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+ 
+
+namespace fotogaleri.Controllers
+{
+    public class GaleriController : Controller
+    {
+        // GET: Galeri
+        public ActionResult Index()
+        {
+
+
+            List<Resim> result = new ResimModel();
+            return View(result);
+        }
+    }
+}
---
+++ Models/Resim.cs
@@ -0,0 +1,10 @@
+﻿
+namespace fotogaleri.Models
+{
+    public class Resim
+    {
+        public string Baslik { get; set; }
+        public string Aciklama { get; set; }
+        public string Url { get; set; }
+    }
+}
